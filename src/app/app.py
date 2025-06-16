b2696a6527bf2c6076a51260ebb1c99fd83a56ed
Primelabs admin <primelabs.admin.git@gmail.com>
2025-06-16 15:16:53 -0700

    basic v1 deployment

---
+++ src/app/app.py
@@ -0,0 +1,147 @@
+from datetime import datetime
+
+import streamlit as st
+
+from data_models import (
+    MedicalRecord,
+    Patient, 
+    Doctor, 
+    MedicalTest,  
+    Payment, 
+    User
+)
+
+CURRENT_USER = User.SUPERVISOR.value
+
+class Form:
+    def __init__(self):
+        pass
+    
+    def render(self):
+        st.header('Medical Test Entry')
+
+        ## Patient Information
+        patient_name = st.text_input(
+                            label="Patient's Name",
+                            label_visibility="hidden",
+                            placeholder="Patient's Name", 
+                            help='Enter the name of the patient')
+        patient = Patient(name=patient_name)
+        
+        phone_col1, phone_col2 = st.columns(2)
+        phone_available = phone_col1.checkbox("Patient's Phone")
+        if phone_available:
+            patient_phone = phone_col2.text_input(
+                            label="Patient's Phone",
+                            label_visibility="hidden",
+                            placeholder="Patient's Phone Number", 
+                            help='Enter the phone number of the patient')
+            patient.phone = patient_phone
+        
+        address_col1, address_col2 = st.columns(2)
+        address_available = address_col1.checkbox("Patient's Address")
+        if address_available:
+            patient_address = address_col2.text_input(
+                            label="Patient's Address",
+                            label_visibility="hidden",
+                            placeholder="Patient's Address", 
+                            help='Enter the address of the patient')
+            patient.address = patient_address
+            
+        
+        ## Referral Information
+        referring_doctor = None
+        referral_col1, referral_col2 = st.columns(2)
+        through_referral = referral_col1.checkbox("Referral")
+        if through_referral:
+            doctor_name = referral_col2.text_input(
+                            label="Doctor's Name",
+                            label_visibility="hidden",
+                            placeholder="Doctor's Name", 
+                            help='Enter the name of the doctor')
+            doctor_location = referral_col2.text_input(
+                            label="Doctor's Location",
+                            label_visibility="hidden",
+                            placeholder="Doctor's Location", 
+                            help='Enter the location of the doctor')
+            referring_doctor = Doctor(name=doctor_name, location=doctor_location)
+        
+        
+
+        TEST_PRICES = {
+            'Blood Test': 200,
+            'Urine Test': 150,
+            'X-Ray': 500,
+            'MRI': 1500
+        }
+        testinfo_col1, testinfo_col2 =  st.columns(2)        
+        test_name = testinfo_col1.selectbox(
+                            label='Test Type',
+                            options=list(TEST_PRICES.keys()),
+                            help='Select the medical test')
+        
+        
+        test_price = testinfo_col2.text_input(
+                            label='Price',
+                            disabled=True,
+                            value =f"{TEST_PRICES[test_name]} Rupees",
+                            )
+        
+        ## Payment Information
+        payment_col1, payment_col2 = st.columns(2)
+        payment_amount  = payment_col1.number_input(
+                            label='Payment',
+                            step=100,
+                            help='Enter the payment amount')
+        payment = Payment(amount=payment_amount)
+        comments = payment_col2.text_area(
+                            label='Comments',
+                            help='Enter any comments')
+
+            
+        submitted = st.button(label='Submit')
+        
+        if submitted:
+            # st.write('Form submitted')
+            # st.write(f'Patient Name: {patient_name}')
+            # if phone_available:
+            #     st.write(f'Patient Phone: {patient_phone}')
+            # if address_available:
+            #     st.write(f'Patient Address: {patient_address}')
+            # if through_referral:
+            #     st.write(f'Referred by Dr. {doctor_name} from {doctor_location}')
+            # st.write(f'Paid {payment} Rupees for {test_name}')
+            # st.write(f'Comments: {comments}')
+            try:
+                medical_entry = MedicalRecord(
+                    patient=patient,
+                    doctor=referring_doctor,
+                    medical_test=MedicalTest(name=test_name, price=TEST_PRICES[test_name]),
+                    payment=payment,
+                    date=datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                    comments=comments,
+                    updated_by=CURRENT_USER
+                )
+                print (medical_entry)
+
+            except Exception as e:
+                st.write(e)
+
+
+class PrimeLabsUI:
+    def __init__(self):
+        pass
+
+    def render(self):
+        with st.sidebar:
+            st.title('PrimeLabs')
+            st.write('Primelabs management system')
+            st.session_state.times_loaded = st.session_state.get('times_loaded', 0)
+            st.session_state.times_loaded += 1
+            st.write(f"Times loaded: {st.session_state.times_loaded}")
+            
+        Form().render()
+
+if __name__ == '__main__':
+    ui = PrimeLabsUI()
+    ui.render()
---
+++ src/app/data_models.py
@@ -0,0 +1,105 @@
+from typing import Optional
+from datetime import datetime
+
+from strenum import StrEnum
+from pydantic import BaseModel, Field
+
+
+class User(StrEnum):
+    MANAGER = "Person 1"
+    SUPERVISOR = "Person 2"
+    DOCTOR = "Person 3"
+    OTHER = "Other"
+
+
+class DatabaseRecord(BaseModel):
+    """
+    Base class for all database records
+    """
+    date: datetime = Field(description="Date time of the record")
+    updated_by: User = Field(description="Last updated by")
+
+
+class Patient(BaseModel):
+    name: str = Field(description="Name of the patient")
+    phone: str = Field(description="Phone number of the patient", default=None)
+    address: str = Field(description="Address of the patient", default=None)
+
+
+class Doctor(BaseModel):
+    name: str = Field(description="Name of the doctor")
+    location: str = Field(description="location of the doctor", default=None)
+    phone: str = Field(description="Phone number of the doctor", default=None)
+    
+
+#TODO: prices do not change so this can be a simple lookup table
+class MedicalTest(BaseModel):
+    name: str = Field(description="Name of the test")
+    price: int = Field(description="Price of the test", default=None)
+
+
+class DoctorReferral(BaseModel):
+    doctor: Doctor
+    medical_test: MedicalTest
+    amount: int = Field(description="Referral amount charged by the doctor")
+
+
+class Payment(BaseModel):
+    amount: int = Field(description="Amount of the payment")
+    description: str = Field(description="Description of the payment", default=None)
+
+
+class MedicalRecord(DatabaseRecord):
+    patient: Patient = Field(description="Patient who underwent the medical test")
+    doctor: Optional[Doctor] = Field(description="Doctor who referred the patient", default=None)
+    medical_test: MedicalTest = Field(description="Medical test performed on the patient")
+    payment: Payment = Field(description="Payment made for the medical test")
+    comments : str = Field(description="Comments on the medical entry", default=None)
+    
+
+class ExpenseType(StrEnum):
+    DOCTOR_FEES = "Doctor Fees"
+    STAFF_EXPENSE = "Staff Expense"
+    EQUIPMENT = "Equipment"
+    RENT = "Rent"
+    SALARY = "Salary"
+    STATIONARY = "Stationary"
+    CHAI_NASHTA = "Chai Nashta"
+    MISCELLANEOUS = "Miscellaneous"
+
+
+class ExpenseRecord(DatabaseRecord):
+    expense_type: ExpenseType = Field(description="Type of expense")
+    amount: int = Field(description="Amount of the expense")
+    description: str = Field(description="Description of the expense", default=None)
+
+
+class LedgerRecord(DatabaseRecord):
+    initiator: User = Field(description="User who initiated the payment")
+    benefactor: User = Field(description="User who collected the payment")
+    amount: int = Field(description="Amount of the payment")
+    description: str = Field(description="Description of the payment", default=None)
+
+
+class ReferralRecord(DatabaseRecord):
+    doctor: Doctor = Field(description="Doctor who referred the patient")
+    medical_test: MedicalTest = Field(description="Medical test that was referred", default=None)
+    patient_amount: Payment = Field(description="Payment made by the patient", default=None)
+    doctor_amount: Payment = Field(description="Referral amount charged by the doctor")
+    comments: str = Field(description="Comments on the referral", default=None)
+
+
+if __name__=="__main__":
+
+    CURRENT_USER = User.SUPERVISOR.value
+    
+    medical_entry = MedicalRecord(
+        patient=Patient(name="John Doe"),
+        doctor=Doctor(name="Dr. Smith", location="Bangalore"),
+        medical_test=MedicalTest(name="Blood Test", price=200),
+        payment=Payment(amount=200),
+        date="2021-01-01",
+        comments="Test comments",
+        updated_by=CURRENT_USER
+    )
+    print (medical_entry.json())
